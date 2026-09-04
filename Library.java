ac8608fadd9ed83b22aa348e06202cb71755b17d
santiago <santiago.loaiza58068@ucaldas.edu.co>
2026-09-04 10:03:56 -0500

    Add publishers collection to Library

--- Library.java
+++ Library.java
@@ -2,5 +2,9 @@ public class Library{
     private Author[] authors = new Author[]{};
 
     private Book[] books = new Book []{};
+
+    private Publisher[] publishers = new Publisher[]{};
+
+    
     
 }
